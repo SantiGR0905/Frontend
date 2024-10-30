a944f2efc1ac8f4365879a1bbfadf0456c83841f
JULIAN SANTIAGO GUTIERREZ RODRIGUEZ <jusantiago10guti@gmail.com>
2024-10-30 12:44:24 -0500

    Update Img

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -13,7 +13,7 @@ function HomePage() {
             <header>
                 <div className='Titulo'>
                     <a href="/">
-                        <img src="src/img/titulo.png" alt="" />
+                        <img src="/img/titulo.png" alt="" />
                     </a>
                 </div>
                 <div className='barra_derecha'>
@@ -27,26 +27,26 @@ function HomePage() {
             <main>
                 <div className='postheader'>
                     <p>Plataforma de visualización para la compra de muebles en línea que brinda una experiencia inmersiva en 3D donde permite a los usuarios personalizar y visualizar muebles en sus propios espacios mediante realidad aumentada y entornos virtuales, buscando facilitar la elección de colores, materiales y disposición de los muebles de forma realista reduciendo así la incertidumbre y errores en las decisiones de compra.</p>
-                    <img src="src/img/homepage.webp" alt="" />
+                    <img src="/img/homepage.webp" alt="" />
                 </div>
                 <h1>Products</h1>
                 <div className='products'>
                     <div className='icono'>
-                        <img src="src/img/mueblebaño.png" alt="" />
+                        <img src="/img/mueblebaño.png" alt="" />
                         <div className='contenido-icono'>
                         <h3>Mueble baño</h3>
                         <p>Mueble De Baño Amaretto Miel 46x 61.5 x 47 cm con Lavamanos Blanco</p>
                         </div>
                     </div>
                     <div className='icono'>
-                        <img src="src/img/mueblebaño.png" alt="" />
+                        <img src="/img/mueblebaño.png" alt="" />
                         <div className='contenido-icono'>
                         <h3>Mueble baño</h3>
                         <p>Mueble De Baño Amaretto Miel 46x 61.5 x 47 cm con Lavamanos Blanco</p>
                         </div>
                     </div>
                     <div className='icono'>
-                        <img src="src/img/mueblebaño.png" alt="" />
+                        <img src="/img/mueblebaño.png" alt="" />
                         <div className='contenido-icono'>
                         <h3>Mueble baño</h3>
                         <p>Mueble De Baño Amaretto Miel 46x 61.5 x 47 cm con Lavamanos Blanco</p>
